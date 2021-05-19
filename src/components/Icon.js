8ef2432bf75fabdb6b96e48027401d1ab3bd5a38
Ericabergman <erikabergman94@gmail.com>
2021-05-19 23:50:21 +0200

    second commit

---
+++ src/components/Icon.js
@@ -0,0 +1,27 @@
+// import React from "react";
+// import "./components/NavBar";
+// import icon from "./icon.png";
+// import NavBar from "./Modal/NavBar";
+
+// const Icon = () => {
+//   return (
+//     <div className="container">
+//       <img src={Iconpng} className="Icon"></img>
+//       {/* <MenuIcon> */}
+//       {/* src={MenuIcon} className={BtnNav} */}
+//       {/* </MenuIcon> */}
+//     </div>
+//   );
+// };
+
+// export defaultc Iconpng
+// --------------------------------------------------------------
+// function Icon() {
+//   return (
+//     <div>
+//       <img src={icon.svg} className="btn" alt="openNavBar" />
+//     </div>
+//   );
+// }
+
+// export default Icon;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./Modal.css";
+
+function Modal() {
+  const [modal, setModal] = useState(false);
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <button onClick={toggle} className="btn_nav">
+        Open Modal
+      </button>
+
+      {modal && (
+        <div className="container-modal">
+          <div className="overlay"></div>
+          <div className="modal_content">
+            <h2>Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+              beatae accusantium asperiores repellendus, neque, itaque ducimus,
+              nihil tempora quisquam fugit assumenda nobis odio fuga ipsam
+              reiciendis. Voluptate ipsam neque repudiandae.
+            </p>
+            <button className="close" onClick={toggle}>
+              Close Modal
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+export default Modal;
